6ff6904d9535781ba918c207e5e0950f31aa97fc
Eetu <99178022+E3TU@users.noreply.github.com>
2025-12-07 00:45:13 +0200

    Init project

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.plugin.serialization)
+}
+
+group = "com"
+version = "0.0.1"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+dependencies {
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.cors)
+    implementation(libs.ktor.server.compression)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.config.yaml)
+    // testImplementation(libs.ktor.server.test.host)
+    // testImplementation(libs.kotlin.test.junit)
+    implementation("io.ktor:ktor-client-cio:3.3.3")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.5.1")
+}
